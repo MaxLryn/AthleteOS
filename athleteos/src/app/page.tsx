b99e7c98f5e3d050f56e643f1958c0080bd24a03
MaxLryn <mlaroyenne2002@gmail.com>
2026-06-06 13:19:17 +0200

    Update page.tsx

--- athleteos/src/app/page.tsx
+++ athleteos/src/app/page.tsx
@@ -1,10 +1,5 @@
 import { redirect } from 'next/navigation'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
-  if (session) redirect('/dashboard')
-  else redirect('/login')
+  redirect('/login')
 }
